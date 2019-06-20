a26410b6dadfa1957d111661d49f695a1df587f8
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-20 18:54:42 +0530

    Update List_Cases.py

--- List_Cases.py
+++ List_Cases.py
@@ -1,1 +1,12 @@
+#List Cases
+list_of_airlines["AirIndia","Emirates","BritishAirways"]
 
+#Iterating the list using range()
+print("Iterating the list using range()")
+     for index in range(0,len(list_of_airlines)):
+         print(list_of_airlines[index])
+      
+#Iterating the list using in keyword
+print("Iterating the list using keyword in")
+     for airline in list_of_airlines:
+         print(airline)
