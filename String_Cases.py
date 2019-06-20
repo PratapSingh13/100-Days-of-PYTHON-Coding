c553e0e951d54024cc113144b30f59e6b2b2d415
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-20 18:18:10 +0530

    Update String_Cases.py

--- String_Cases.py
+++ String_Cases.py
@@ -19,3 +19,17 @@ print(Length of the PAN Card Number is:",len(pancard_number))
       print("Iterating the String using keyword in")
       for value in pancard_number:
       print(value)
+#Searching for Character in String
+      print("Searching for the Character in String")
+      if "Z" in pancard_number:
+          print("Character Is Present")
+      else:
+          print("Character is not Present")
+
+#Slicing a String
+      print("The Number in PAN Card is:",pancard_number[5:9])
+      print("Last but one 3 Characters in PAN Card:",pancard_number[-4:-1]
+            
+      
+      
+  
